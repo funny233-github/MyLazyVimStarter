b46bf012a68cc9f1bfab2844867b126eb2f71ead
funny_233 <83470506+funny233-github@users.noreply.github.com>
2025-02-17 18:00:01 +0800

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,64 +1,57 @@
-local function avante_opts(endpoint, model)
-  return {
-    provider = "openai",
-    openai = {
-      endpoint = endpoint,
-      model = model,
-      timeout = 30000,
-      temperature = 0,
-      max_tokens = 4096,
-    },
-  }
-end
-
-local avante_dependencies = {
-  "stevearc/dressing.nvim",
-  "nvim-lua/plenary.nvim",
-  "MunifTanjim/nui.nvim",
-  --- The below dependencies are optional,
-  "echasnovski/mini.pick", -- for file_selector provider mini.pick
-  "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-  "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-  "ibhagwan/fzf-lua", -- for file_selector provider fzf
-  "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  "zbirenbaum/copilot.lua", -- for providers='copilot'
-  {
-    -- support for image pasting
-    "HakonHarnes/img-clip.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- recommended settings
-      default = {
-        embed_image_as_base64 = false,
-        prompt_for_file_name = false,
-        drag_and_drop = {
-          insert_mode = true,
-        },
-        -- required for Windows users
-        use_absolute_path = true,
-      },
-    },
-  },
-  {
-    -- Make sure to set this up properly if you have lazy=true
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      file_types = { "markdown", "Avante" },
-    },
-    ft = { "markdown", "Avante" },
-  },
-}
-
 return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = avante_opts("http://localhost:11434/v1", "deepseek-r1:8b"),
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          disable_tools = true,
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.siliconflow.cn",
+          model = "Pro/deepseek-ai/DeepSeek-R1",
+        },
+      },
+    },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = avante_dependencies,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   },
 }
